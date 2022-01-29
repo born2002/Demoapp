866595ced96bf96545ee07273b7003238874394b
born.2002 <itiiphon@gmail.com>
2022-01-29 09:57:54 +0700

    first commit

---
+++ Demoapp.php
@@ -0,0 +1,9 @@
+<?php
+ $name = array("hon", "mun", "not", "kar", "fim", "mot");
+ $age = array(20, 25, 18, 15, 22);
+ echo "ชื่อ $name[0] อายุ $age[0] <br>";
+ echo "ชื่อ $name[1] อายุ $age[1] <br>"; 
+ echo "ชื่อ $name[2] อายุ $age[2] <br>";
+ echo "ชื่อ $name[3] อายุ $age[3] <br>";
+ echo "ชื่อ $name[4] อายุ $age[4] <br>";
+?>
